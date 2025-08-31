e2e67fe729d8d514d411de755b4cc77cfbdc977d
Ligxigy <lxy0105jntm@outlook.com>
2025-08-31 13:47:26 +0800

    ArrayList提交

---
+++ src/ArrayList/ArrayListt.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+public interface ArrayListt {
+
+    void InitArray();
+
+    boolean EndAdd(int k);
+
+    boolean MiddleAdd(int k, int targer);
+
+    boolean HeadAdd(int k);
+
+    int Find(int k);
+
+    boolean Dalect(int k);
+
+    boolean Revise(int k, int targer);
+
+    void Print();
+
+}
