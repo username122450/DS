a77ebf73f54442b74b47ef420900780870578900
刘星宇 <16112468+two-lanes-of-rain@user.noreply.gitee.com>
2025-11-01 13:26:08 +0800

    matrix:新增邻接矩阵的深度优先搜索的实现

--- graph/matrix.c
+++ graph/matrix.c
@@ -4,6 +4,7 @@
 
 
 //邻接矩阵实现有向，无向，有权，无权的图的存储
+int flag[105];
 
 typedef struct{
     char *v;
@@ -33,6 +34,16 @@ int Find(char *graph,char data,int len){
         if(graph[i] == data) return i;
     }
     return -1;
+}
+
+void DFS(Matrix *matrix,int x){
+    flag[x] = 1;
+    printf("%c",matrix->v[x]);
+    for(int i = 0;i<matrix->len;i++){
+        if(matrix->matrix[x][i] == 1 && flag[i]== 0){
+            DFS(matrix,i);
+        }
+    }
 }
 
 int main(){
@@ -89,16 +100,32 @@ int main(){
             matrix->matrix[yi][xi] = weight;
         }
     }else{
-        //无向无权图
+        printf("无向无权图\n");
         for(int i = 1; i<= num;i++){
             printf("输入第%d条边的描述：\n",i);
             char x,y;
-            scanf(" %c %c\n", &x,&y);
+            getchar();
+            scanf("%c %c",&x,&y);
             int xi = Find(matrix->v,x,len);
+            printf("%c %d\n",x,xi);
             int yi = Find(matrix->v,y,len);
+            printf("%c %d\n",y,yi);
             matrix->matrix[xi][yi] = 1;
             matrix->matrix[yi][xi] = 1;
         }
     }
+    DFS(matrix,0);
     return 0;
 }
+/*
+4
+0 0
+a b c d
+5
+a b
+a c
+a d
+b c
+d c
+
+*/
