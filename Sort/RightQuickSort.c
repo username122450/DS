a162e9bb6e1077c2068ef6a7a0af4790bfd34c43
刘星宇 <16112468+two-lanes-of-rain@user.noreply.gitee.com>
2026-01-28 00:06:47 +0800

    QuickSort:提交基于数组的左,右基准的快速排序的完成文件

---
+++ Sort/RightQuickSort.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// This is the code for Right Quick Sort --260127
+
+void QSort(int *nums,int l,int r){
+    if(l < r){
+        int temp = nums[r];
+        int i = l,j = r;
+        while(i < j){
+            while(i < j && nums[i] < temp)i++;
+            if(i < j){
+                nums[j] = nums[i];
+                j--;
+            }
+            while(i < j && nums[j] > temp) j--;
+            if(i < j){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        nums[i] = temp;
+        QSort(nums,l,i-1);
+        QSort(nums,i+1,r);
+    }
+}
+
+int main(){
+    int num;
+    printf("Please enter the quantity of these numbers:");
+    scanf(" %d", &num);
+    int *nums = (int *)malloc(sizeof(int )* num);
+    for(int i = 0;i < num;i++){
+        scanf(" %d",&nums[i]);
+    }
+
+    QSort(nums,0,num-1);
+
+    printf("Sorting completed!\n");
+    for(int i = 0;i < num;i++){
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
+    printf("Output completed!\n");
+    return 0;
+}
